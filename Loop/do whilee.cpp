86624381eb0d9260ee4c2497192ffb02bc2e8967
Sourav Mukharjee <115613536+SouravMukharjee@users.noreply.github.com>
2025-07-18 17:25:32 +0300

    Add files via upload

---
+++ Loop/do whilee.cpp
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	int i;
+	do{
+	
+	for(i=17;i<100;i=i+1)
+		printf("%d\t",i);
+
+	if(i==17){
+		printf("%d\t",i);
+	}
+}
+while(i!=-40);
+	getch();
+	return 0;
+}
+
